c72ada6d9075776cbcecf0f7e2e2728263285c53
Jericho Ramos <jericho@tatakstudio.co>
2024-08-29 23:11:14 +0800

    Add basic math functions
    Calculator will not function without the basic math operations

---
+++ calculator/main.js
@@ -0,0 +1,19 @@
+let firstNumber, secondNumber, operator;
+
+function operate(firstNumber, secondNumber, operator) {}
+
+function add(a, b) {
+	return a + b;
+}
+
+function subtract(a, b) {
+	return a - b;
+}
+
+function multiply(a, b) {
+	return a * b;
+}
+
+function divide(a, b) {
+	return a / b;
+}
