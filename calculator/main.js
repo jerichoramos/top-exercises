5fcd16a55323f07ee3d49f0fee23816c29fa2ca0
Jericho Ramos <jericho@tatakstudio.co>
2024-08-30 23:01:20 +0800

    Add the operate function

--- calculator/main.js
+++ calculator/main.js
@@ -1,8 +1,24 @@
+let displayScreen = document.querySelector(".display");
 let firstNumber, secondNumber, operator;
 
-function operate(firstNumber, secondNumber, operator) {}
+function operate(firstNumber, secondNumber, operator) {
+	switch (operator) {
+		case "+":
+			add(firstNumber, secondNumber);
+			break;
+		case "-":
+			subtract(firstNumber, secondNumber);
+			break;
+		case "x":
+			multiply(firstNumber, secondNumber);
+			break;
+		case "/":
+			divide(firstNumber, secondNumber);
+	}
+}
 
 function add(a, b) {
+	console.log(a + b);
 	return a + b;
 }
 
@@ -17,3 +33,9 @@ function multiply(a, b) {
 function divide(a, b) {
 	return a / b;
 }
+
+function displayNumber() {
+	let number;
+}
+
+displayNumber();
